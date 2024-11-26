79b4c895816b4528bd576fa842a9d87261cfdc31
Labib1851 <labibabeed2007@gmail.com>
2024-11-26 12:07:28 +0600

    add new file

---
+++ script.js
@@ -0,0 +1,22 @@
+// create a simple calculator
+
+console.log("2+2 =" + (2+2));
+console.log("10-5 =" + (10-5) );
+console.log("2*3 =" + (2*3));
+console.log("12/2 =" + (12/2));
+console.log("5%2 =" + (5%2));
+
+console.log(`first+second`);
+
+const firstnumber = 10;
+const secondnumber = 5;
+console.log(`${firstnumber} + ${secondnumber} = ${firstnumber + secondnumber} `);
+
+const firstnumber = promet('enter frist number:')
+const fristnumber = promet('enter second number:')
+
+const firstnumber = number(prompt('entr frist number: '));
+const secondnumber = number(prompt('enter the second number: '))
+
+
+
